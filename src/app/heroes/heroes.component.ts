59ccbf6d3fac98bf3e8c5110c87577903c38a6f0
Yogeshwar Jaganathan <yojaganathan@BLR.ALLEGISINDIA.COM>
2020-01-28 19:17:37 +0530

    RoutingModule implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,7 +17,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero : Hero): void {
