00e6b9bf2df49488dee789606dd541ee2ca25183
yogeshj939 <32947095+yogeshj939@users.noreply.github.com>
2020-01-29 00:04:54 +0530

    unwanted commit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,12 +17,12 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero : Hero): void {
     this.selectedHero = hero;
-
   }
 
 }
